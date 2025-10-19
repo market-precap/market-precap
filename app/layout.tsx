0f467d557ded25ff83ce4e66fc7dd2705a6271ee
Mohit <chaudharimohit39@gmail.com>
2025-10-19 20:01:09 +0530

    Added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import MainLayout from "../components/layout/MainLayout";
 import "./globals.css";
 
@@ -68,6 +69,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-white dark:bg-gray-900`}
       >
         <MainLayout>{children}</MainLayout>
+        <SpeedInsights />
       </body>
     </html>
   );
